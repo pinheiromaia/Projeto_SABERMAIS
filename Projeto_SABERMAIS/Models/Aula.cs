77efbce4c2db00a505d13ad780353f0437acb920
Claudia Marcela Pinheiro Maia <marcelapmaia@gmail.com>
2025-10-30 22:18:14 +0000

    Remove Aula class from the Models directory

--- Projeto_SABERMAIS/Models/Aula.cs
+++ Projeto_SABERMAIS/Models/Aula.cs
@@ -1,14 +1,1 @@
-﻿namespace Projeto_SABERMAIS.Models
-{
-    public class Aula
-    {
-        public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string Descricao { get; set; }
-        public string VideoUrl { get; set; }
 
-        // Uma aula pode ter várias perguntas
-        public List<Pergunta> Perguntas { get; set; } = new List<Pergunta>();
-    }
-}
-   
